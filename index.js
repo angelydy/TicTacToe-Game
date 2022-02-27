0171b4b4906da475676c8547aa5a5722d02f1248
angely <angelymaedyy@gmail.com>
2022-02-28 00:20:19 +0800

    added keyfraames

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var body = document.body
 
 //check who will play first and display
-var askContainer = document.getElementById('ask-player')
+const askContainer = document.getElementById('ask-player')
+const tileContainer = document.getElementById('tile-container')
 const xBtn = document.getElementById('x-button')
 const yBtn  = document.getElementById('y-button')
 function displayCurrentPlayer() {
   if (askContainer.style.display === "block") {
     askContainer.style.display = "none"
+    tileContainer.style.animation = 'fromTop 2s ease'
   } else {
     askContainer.style.display = "block"
   }

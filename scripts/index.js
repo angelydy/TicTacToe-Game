191b2ee9d3d8af57655b05a1d63cbe16769d2ddb
angely <angelymaedyy@gmail.com>
2022-03-01 22:09:03 +0800

    added folder for sass

--- scripts/index.js
+++ scripts/index.js
@@ -6,10 +6,15 @@ const playerOne = document.getElementById('current-player')
 const playerTwo = document.getElementById('current-player-two')
 const playerOneName = document.getElementById('player-one-name')
 const playerTwoName = document.getElementById('player-two-name')
+const winnerContainer = document.getElementById('show-winner')
+const winnerText = document.getElementById('winner-text')
 
 let currentPlayer = 'X'
 let board = ['','','','','','','','','']
 let activeGame = true
+const PlayerOWon = 'Player O Won'
+const PlayerXWon = 'Player X Won'
+const Tie = 'TIE'
 
 //check who will play first and display
 const askContainer = document.getElementById('ask-player')
@@ -18,6 +23,9 @@ const xBtn = document.getElementById('x-button')
 const oBtn  = document.getElementById('o-button')
 
 function chooseFirstPlayer() {
+  if (askContainer.style.display === 'block') {
+    activeGame = false
+  }
   function displayPlayerOne() {
     askContainer.style.display = "none"
     tileContainer.style.animation = 'fromTop 2s ease'
@@ -51,11 +59,87 @@ tiles.forEach((tile, index) => {
   tile.addEventListener('click', () => playerAction(tile,index))
 })
 
+
+//for checking the winner
+const winningConditions = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+function checkWin() {
+  let winner = false;
+  for (let i = 0; i <= 7; i++) {
+    const win = winningConditions[i]
+    const x = board[win[0]]
+    const y = board[win[1]]
+    const z = board[win[2]]
+    if (x === '' || y === '' || z === '') {
+      continue
+    }
+    if (x === y && y === z) {
+      winner = true
+      break
+    }
+  }
+
+  if (winner) {
+    showWinner(currentPlayer === 'X' ? PlayerXWon : PlayerOWon)
+    activeGame = false;
+    return
+  }
+
+  if (!board.includes(''))
+  showWinner(Tie)
+}
+
+const showWinner = (player) => {
+  winnerContainer.style.display = 'flex'
+  switch (player) {
+    case PlayerOWon:
+      winnerText.textContent = `Player O Won`
+      break;
+    case PlayerXWon:
+      winnerText.textContent = `Player X Won`
+      break;
+    case Tie:
+      winnerText.textContent = `TIE!`
+  }
+}
+
+//check if tile have content or not
 const checkAction = (tile) => {
   if (tile.textContent === 'X' || tile.textContent === 'O') {
     return false;
   }
   return true
+}
+
+//updates the board
+const viewBoard = (index) => {
+  board[index] = currentPlayer
+}
+
+//change current player
+const changePlayer = () => {
+  if (currentPlayer === 'X') {
+    playerTwoContainer.style.border = '2px solid white'
+    playerTwo.textContent = `Player O is Playing`
+    playerOne.textContent = `Player X`
+    playerOneContainer.style.border = 'none'
+    currentPlayer = 'O'
+  } else {
+    playerOneContainer.style.border = '2px solid white'
+    playerOne.textContent = `Player X is Playing`
+    playerTwo.textContent = `Player O`
+    playerTwoContainer.style.border = 'none'
+    currentPlayer = 'X'
+  }
 }
 
 //game in action 
@@ -65,24 +149,6 @@ const playerAction = (tile, index) => {
     viewBoard(index)
     checkWin()
     changePlayer()
-  }
-}
-
-const viewBoard = (index) => {
-  board[index] = currentPlayer
-}
-
-//change current player
-const changePlayer = () => {
-  currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
-  if (currentPlayer === 'X') {
-    playerOneContainer.style.border = '2px solid white'
-    playerOne.textContent = `Player X is Playing`
-    playerTwo.textContent = `Player O`
-  } else {
-    playerTwoContainer.style.border = '2px solid white'
-    playerTwo.textContent = `Player O is Playing`
-    playerOne.textContent = `Player X`
   }
 }
 
@@ -105,6 +171,8 @@ function reset() {
     playerTwoContainer.style.border = 'none'
     playerOne.textContent = `Player X`
     playerTwo.textContent = `Player O`
+    playerOneName.textContent = 'nickname';
+    playerTwoName.textContent = 'nickname';
     board = ['','','','','','','','','']
     activeGame = true
     askContainer.style.display = 'block'
